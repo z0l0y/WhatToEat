0f7723bebd21e235ebe04c68e4e90c913280c3eb
z0l0y <421634412@qq.com>
2024-09-11 13:00:31 +0800

    chore(pom): Add necessary dependencies for the project

---
+++ src/main/java/com/hust/whattoeat/WhatToEatApplication.java
@@ -0,0 +1,13 @@
+package com.hust.whattoeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatToEatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatToEatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hust/whattoeat/WhatToEatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hust.whattoeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatToEatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
